e87d1ef4a1bdd2ef44fbb93a405696af3ec4a1aa
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2023-07-27 16:11:58 +0200

    3-3 - początek

---
+++ matinf/ZESTAW3/zad_3-Liceum/dane.py
@@ -0,0 +1,19 @@
+# opis przedmiotów
+# matematyka (mat), język polski (jpo), język angielski (jan), język hiszpański (jhi), historia (his), biologia (bio), fizyka (fiz), chemia (che), informatyka (inf), wiedza o społeczeństwie (wos), geografia (geo), podstawy przedsiębiorczości (prz).
+
+# budujemy słownik -> klucz:wartość
+
+przedmioty = {
+    "mat": "matematyka",
+    "jpo": "język polski",
+    "jan": "język angielski",
+    "jhi": "język hiszpański",
+    "his": "historia",
+    "bio": "biologia",
+    "fiz": "fizyka",
+    "che": "chemia",
+    "inf": "informatyka",
+    "wos": "wiedza o społeczeństwie",
+    "geo": "geografia",
+    "prz": "podstawy przedsiębiorczości",
+}
---
+++ matinf/ZESTAW3/zad_3-Liceum/zadania.py
@@ -0,0 +1,40 @@
+from dane import *
+
+with open("oceny.txt") as dane:
+    dane_wczytane = dane.readlines()
+
+# print(dane_wczytane)
+# przykładowe dane:
+# [
+# 'OU geo 6\n',
+# 'EP jpo 6 3 2 5 5 6 1\n',
+# 'OH fiz 1 4 1 1 4\n',
+# 'JO wos 2 6 5 4 2 6\n'
+# ]
+
+# teraz czyścimy wstępnie
+dane_czyste = []
+for wiersz in dane_wczytane:
+    dane_czyste.append(wiersz.strip().split())
+
+# print(dane_czyste)
+# przykładowe dane:
+# [
+# ['OU', 'geo', '6'],
+# ['EP', 'jpo', '6', '3', '2', '5', '5', '6', '1'],
+# ['OH', 'fiz', '1', '4', '1', '1', '4'],
+# ['JO', 'wos', '2', '6', '5', '4', '2', '6']
+# ]
+
+print(f"Łącznie wczytano danych: {len(dane_czyste)}.")
+
+
+# teraz zaczniemy odpowiedzi na pytania
+
+bez_ocen = 0
+for element in dane_czyste:
+    if len(element) == 2:
+        bez_ocen += 1
+        print(f"Bez ocen osoba: {element[0]}")
+else:
+    print(f"Bez ocen osób: {bez_ocen}")
